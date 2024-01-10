bd6beb498b66f08c4cdcf5c53343860a0af7f67e
Larry-King-9 <1505869020@qq.com>
2024-01-10 22:12:40 +0800

    leftsliding finish

--- src/pages/FoodList/FoodListTest.vue
+++ src/pages/FoodList/FoodListTest.vue
@@ -1,19 +1,19 @@
 <template>
 	<h1>this is FoodList test</h1> <br>
+	<u-tabs :list="list_navigation" @click="click_navigation"></u-tabs>
 	<!-- <h1>this is abandon</h1> -->
 	<search :placeholder="SearchPlaceholder" @SearchMessage="searchMessage"></search>
 	<navigationBar></navigationBar>
 	<FoodListCard :FoodName="FoodListCardName" style="margin-top: 15px;"></FoodListCard>
-	<FoodListCard :FoodName="FoodListCardName"></FoodListCard>
-
-
-
-	<!-- <button @click="sendGet()">this is a button</button> -->
-
+	<!--  style="margin-top: 15px;" -->
+	<FoodListCard FoodName="香肠"></FoodListCard>
 	
-
-
-
+	<div v-for="(food, index) in food_name" :key="index">
+		<FoodListCard :FoodName="food"></FoodListCard>
+	</div>
+	
+	
+	
 </template>
 
 <script>
@@ -28,53 +28,119 @@
 	import search from "@/components/search-component.vue"
 	import FoodListCard from "@/components/FoodListCard-component.vue"
 
+	import leftSliding from "@/components/leftSliding-component.vue"
+
 	export default {
 		components: {
 			navigationBar,
 			search,
 			FoodListCard,
+			leftSliding,
+
 
 
 		},
 		data() {
 			return {
 				value3: '',
-
-
+				list_navigation: [
+					{
+						name: "所有"
+					},
+					{
+						name: "蔬菜"
+					},
+					{
+						name: "肉类"
+					},
+					{
+						name: "主食"
+					},
+					{
+						name: "水果"
+					},
+					{
+						name: "海鲜"
+					},
+					{
+						name: "调料"
+					},
+					{
+						name: "豆类"
+					},
+					{
+						name: "油"
+					},
+					{
+						name: "其他"
+					}
+				],
+				name_navigation: '所有',
+				index_navigation: 0,
+				food_name: [],
 			}
 		},
 		methods: {
-			goToexa() {
-				uni.navigateTo({
-					url: '/pages/example/example',
-				})
-			},
-			sendGet() {
-				testUser.getTestInfo()
-					.then(response => {
-						// 处理返回的数据
-						console.log('success');
-						console.log('Response:', response.data);
-						console.log('Response:', response.code);
-						console.log('Response:', response.msg);
-					})
-					.catch(error => {
-						// 处理请求错误
-						console.error('Error:', error);
-					});
-			},
-			//将参数传给search的placeholer
+			//将参数传给search的placeholer, 用于显示默认状态
 			setSearchData() {
 				this.SearchPlaceholder = '搜索食材';
 				this.FoodListCardName = '白菜';
 			},
+			//点击搜索调用的函数, 搜索食材点击后的效果
 			searchMessage(message) {
 				console.log(message) // 'Hello World!'
+			},
+			//从后端获取导航栏的值, 暂时不用
+			getNavigation() {
+				//模仿传入数据
+				let jsonObject = {
+					code: 200,
+					msg: '',
+					data: [
+						"关注",
+						"推荐",
+						"音乐",
+						"电影",
+						"科技",
+						"美食",
+						"文化",
+						"财经",
+						"手工"
+					]
+				};
+				//变成json
+				let jsonString2 = JSON.stringify(jsonObject);
+				//解析json
+				let result = JSON.parse(jsonString2);
+				// 从对象中获取 data 属性的值
+				let data = result.data;
+				console.log('this is data', data);
+				this.list_navigation = data.map(item => {
+				  return { name: item };
+				});
+			},
+			//导航栏点击, 用于获取顶部导航栏状态
+			click_navigation(item){
+				console.log('item', item);
+				this.name_navigation = item.name;
+				this.index_navigation = item.index;
+				console.log(this.name_navigation, this.index_navigation);
+			},
+			//从后端获取数据
+			getData(){
+				this.food_name = [
+					'白菜',
+					'香肠',
+				]
+				console.log(this.food_name)
 			}
 		},
 
+
 		onLoad() {
 			this.setSearchData();
+			this.getData()
+			// this.getNavigation();
 		},
 
 	}
---
+++ src/pages/example/test3.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <img src="../../static/cabbage.jpeg" alt="Your Image" class="image" />
+    <p class="centered-text">Your Text Here</p>
+  </div>
+</template>
+
+<style>
+.container {
+  position: relative; /* 让容器成为定位上下文 */
+  width: 300px; /* 容器宽度 */
+  height: 200px; /* 容器高度 */
+  border: 1px solid #ccc; /* 边框，可选 */
+}
+
+.image {
+  width: 100%; /* 图片宽度充满容器 */
+  height: 100%; /* 图片高度充满容器 */
+  object-fit: cover; /* 图片填充方式，保持比例并填满容器 */
+  position: absolute; /* 图片绝对定位 */
+  top: 0;
+  left: 0;
+}
+
+.centered-text {
+  position: absolute; /* 文本绝对定位 */
+  top: 50%; /* 将文本上边缘置于容器的中间位置 */
+  left: 50%; /* 将文本左边缘置于容器的中间位置 */
+  transform: translate(-50%, -50%); /* 使用 transform 属性偏移文本的位置，使其在容器中垂直和水平方向上居中 */
+  text-align: center; /* 文本水平居中 */
+  z-index: 1; /* 设置层叠顺序，确保文本位于图片之上 */
+}
+</style>
